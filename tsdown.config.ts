17b892920a2503af2b918c575801d64dd0260b8c
ZhouQiJun <zhouqijun4job@163.com>
2025-09-23 04:17:28 +0800

    build: 输出多种格式

--- tsdown.config.ts
+++ tsdown.config.ts
@@ -1,9 +1,15 @@
 import { defineConfig } from 'tsdown'
 
-export default defineConfig([
-  {
-    entry: ['./src/index.ts'],
-    platform: 'neutral',
-    dts: true,
+export default defineConfig({
+  entry: ['./src/index.ts'],
+  platform: 'neutral',
+  format: ['esm', 'cjs', 'iife'],
+  dts: true,
+  globalName: 'olGeomEditor', // 👈 浏览器全局变量名 window.olGeomEditor
+  external: [/^ol($|\/)/], // 👈 不要打进 bundle
+  outputOptions: {
+    globals: {
+      ol: 'ol',
+    },
   },
-])
+})
