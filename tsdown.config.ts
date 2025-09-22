3730302e66fc557db730b0805156b3026af877fc
ZhouQiJun <zhouqijun4job@163.com>
2025-09-23 04:22:04 +0800

    build: 输出 esm 和 iffe

--- tsdown.config.ts
+++ tsdown.config.ts
@@ -3,7 +3,7 @@ import { defineConfig } from 'tsdown'
 export default defineConfig({
   entry: ['./src/index.ts'],
   platform: 'neutral',
-  format: ['esm', 'cjs', 'iife'],
+  format: ['esm', 'iife'],
   dts: true,
   globalName: 'olGeomEditor', // 👈 浏览器全局变量名 window.olGeomEditor
   external: [/^ol($|\/)/], // 👈 不要打进 bundle
