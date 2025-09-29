989773f7e03e1921c13da03e94d53d64dd3afe24
jackchoumine <jackchoumine@gmail.com>
2025-09-29 11:11:34 +0800

    chore: 导出版本号

--- src/index.ts
+++ src/index.ts
@@ -2,8 +2,11 @@
  * @Author      : ZhouQiJun
  * @Date        : 2025-09-08 01:37:38
  * @LastEditors : ZhouQiJun
- * @LastEditTime: 2025-09-08 01:57:04
+ * @LastEditTime: 2025-09-29 10:47:58
  * @Description : 导出接口
  */
+import { version as versionNo } from '../package.json'
+
+export const version = versionNo
 export * from './GeomEditor'
 export * from './utils'
